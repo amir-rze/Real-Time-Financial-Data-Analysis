bbc8fa008a60d52cc58d1f0a22dd477de64e1333
Faraz Dastourian <faraz.dastourian.jamnani1377@gmail.com>
2024-02-03 10:49:32 +0330

    Remove unnecessary comments

--- Notification Service/main.py
+++ Notification Service/main.py
@@ -9,59 +9,6 @@ clients = []
 
 logging.basicConfig(level=logging.INFO)
 
-# port = 12345
-
-# async def read_and_send_data():
-
-#     loop = asyncio.get_event_loop()
-#     consumer = AIOKafkaConsumer(
-#         SIGNAL_KAFKA_TOPIC,loop=loop,
-#         bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-#         value_deserializer=lambda m: json.loads(m.decode('utf-8'))
-#     )
-#     await consumer.start()
-#     try:
-#         async for message in consumer:
-#             data = message.value
-#             logging.info("Notification Service received data ! ")
-#             for client in clients:
-#                 msg = f"{data['signal']} {data['stock_symbol']} shares !!! "
-#                 client.sendall(msg.encode('utf-8'))
-#                 logging.info(f"signal sent to client {client} ! ")
-
-#     finally:
-#         consumer.stop()
-
-
-# def accept_connections(ServerSocket):
-#     while True:
-#         Client, address = ServerSocket.accept()
-#         logging.info('Connected to: ' + address[0] + ':' + str(address[1]))
-#         clients.append(Client)
-
-
-# async def start_server(host, port):
-#     ServerSocket = socket.socket()
-#     try:
-#         ServerSocket.bind((host, port))
-#     except socket.error as e:
-#         logging.error(str(e))
-
-    
-#     logging.info(f'Server is listing on the port {port}...')
-#     ServerSocket.listen(5)
-#     await accept_connections(ServerSocket)
-
-
-# async def main():
-#     # Start the server and the read/send coroutine concurrently
-#     await asyncio.gather(
-#         start_server('localhost', 12345),
-#         read_and_send_data(),
-#     )
-
-# if __name__ == '__main__':
-#     asyncio.run(main())
 
 
 
